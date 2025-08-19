1f535a01b2fc24a463973ada4b50afbfa7f0ddde
JainiSolanki <23cs096@charusat.edu.in>
2025-08-19 18:10:54 +0530

    CRUD

---
+++ models/Product.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+
+var ProductSchema = mongoose.Schema({
+    pname:String,
+    pdetails:String,
+    pprice:Number
+})
+
+var ProductModel = mongoose.model('products',ProductSchema)
+module.exports = ProductModel;
---
+++ routes/index.js
@@ -0,0 +1,77 @@
+var express = require('express');
+var router = express.Router();
+var ProductModel = require('../models/Product')
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/add-product', function(req, res, next) {
+  res.render('add-product')
+});
+
+router.post('/add-product-process', function(req, res, next) {
+  var obj = {
+    pname:req.body.txt1,
+    pdetails:req.body.txt2,
+    pprice:req.body.txt3
+  }
+  var ProductData = new ProductModel(obj)
+  ProductData.save()
+  .then(()=>{
+    console.log("Data Added")
+    res.send("Working")
+  })
+  .catch((err)=>console.log("Error "+err))  
+});
+
+router.get('/display-product', function(req, res, next) {
+  ProductModel.find()
+  .then((mydata)=>{
+    console.log(mydata)
+    res.render('display-product', {mydata:mydata})
+  })
+  .catch((err)=>console.log("Error in display product"))
+})
+
+// Correct route
+router.get('/delete-product/:id', async (req, res) => {
+  try {
+    const id = req.params.id;
+    await ProductModel.findByIdAndDelete(id);
+    res.redirect('/display-product');
+  } catch (err) {
+    console.error("Error in deleting product", err);
+    res.status(500).send("Error in deleting product");
+  }
+});
+
+router.get('/edit-product/:id', function(req, res, next) {
+  var myid = req.params.id
+  ProductModel.findById(myid)
+  .then((mydata)=>{
+    console.log(mydata)
+    res.render('edit-product', {mydata : mydata})
+  })
+  .catch((err)=>console.log("Error in editing product" +err))
+})
+
+router.post('/update-product-process/:id', function(req, res, next) {
+  var myid = req.params.id
+  var mydata = {
+    pname:req.body.txt1,
+    pdetails:req.body.txt2,
+    pprice:req.body.txt3
+  }
+
+  ProductModel.findByIdAndUpdate(myid, mydata)
+  .then(()=>{
+    res.redirect('/display-product')
+  })
+  .catch((err)=>console.log("Error in update" +err))
+  
+})
+
+
+module.exports = router;
