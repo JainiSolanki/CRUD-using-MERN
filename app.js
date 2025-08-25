f0a8cd0776281c6ba1c0703eecf481e9b1959397
JainiSolanki <23cs096@charusat.edu.in>
2025-08-25 13:45:02 +0530

    Port changes

--- app.js
+++ app.js
@@ -44,4 +44,11 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
+
+
 module.exports = app;
